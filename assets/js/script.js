7312fef1bede16b03828594ac00b02bf83545a3e
Aaron Deas <deas.aaron@gmail.com>
2021-05-20 02:07:17 -0400

    Completed more game logic, made it so game can begin

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,23 +16,57 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
-var time = 100;
-var score = 0;
-var highScore = 0;
 var startButton = document.getElementById("start__button");
+startButton.addEventListener("click", startGame);
+var answerButtons = document.querySelector(".answerbutton__container");
+var questionContainer = document.getElementById("questions__container");
+var questionEl = document.getElementById("question");
+var questionIndex;
 
-function startGame(event) {
-	console.log("Start Quiz!");
-	var element = event.target;
-	var state = element.getAttribute("data-state");
-	console.log(state, element);
-	if (state === "unclicked") {
-		startButton.innerText = "Test";
+function startGame() {
+	questionIndex = 0;
+	console.log("Start Game");
+	startButton.classList.add("hide");
+	for (var i = 0; i < 4; i++) {
+		var buttonsElchildren = document.querySelector(
+			".answerbutton__container"
+		).children;
+		buttonsElchildren[i].classList.remove("hide");
+	}
+	nextQuestion();
+}
+
+function nextQuestion() {
+	showQuestion(questionBank[questionIndex]);
+}
+
+function showQuestion(question) {
+	var h2El = document.createElement("h2");
+	questionEl.innerText = questionBank[questionIndex].question;
+
+	for (var i = 0; i <= questionBank[questionIndex].answers.length; i++) {
+		console.log("length", questionBank[questionIndex].answers.length, "i:", i);
+		var button = document.createElement("button");
+		button.innerText = questionBank[0].answers[0].answer;
+		button.classList.add("btn");
+		button.addEventListener("click", chooseAnswer());
+		answerButtons.appendChild(button);
 	}
 }
 
-// function nextQuestion() {}
+function resetButtonState() {
+	next;
+}
+function chooseAnswer() {}
 
-// function gameOver() {}
-
-startButton.addEventListener("click", startGame);
+var questionBank = [
+	{
+		question: "What is 2 + 2?",
+		answers: [
+			{ answer: "One", correct: false },
+			{ answer: "2", correct: false },
+			{ answer: "3", correct: false },
+			{ answer: "4", correct: true },
+		],
+	},
+];
