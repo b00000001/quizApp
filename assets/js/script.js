5aa5fe1e161a6cb7167da1c6cb2916c69bf0592b
Aaron Deas <deas.aaron@gmail.com>
2021-05-20 21:21:38 -0400

    More changes to logic for quiz app, quiz logic mostly worked out

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,18 @@ var questionBank = [
 	},
 	{
 		title: "What color is the Sky?",
-		choices: ["1", "2", "3", "4"],
-		answer: "4",
+		choices: ["Blue", "Red", "Orange", "Pink"],
+		answer: "Blue",
 	},
 	{
 		title: "What is H20?",
-		choices: ["1", "2", "3", "4"],
-		answer: "4",
+		choices: ["Fire", "Earth", "Water", "Air"],
+		answer: "Water",
 	},
 	{
 		title: "What language is this written in?",
-		choices: ["1", "2", "3", "4"],
-		answer: "4",
+		choices: ["C++", "Rubyy", "Javascript", "Perl"],
+		answer: "Javascript",
 	},
 ];
 
@@ -36,6 +36,16 @@ var penaltyTime = 0;
 var penaltyAmount = 5;
 
 startgameTime.addEventListener("click", function () {
+	if (penaltyTime === 0) {
+		penaltyTime = setInterval(function () {
+			secondsRemaining--;
+			timerCurrent.textContent = "Time: " + secondsRemaining;
+			if (secondsRemaining <= 0) {
+				clearInterval(penaltyTime);
+				gameOver();
+			}
+		}, 1000);
+	}
 	console.log("Start game");
 	showQuestions();
 });
@@ -64,19 +74,18 @@ function checkAnswer(event) {
 		console.log("correct");
 		score++;
 		makedivEl.textContent = "That is correct!";
-		questionsContainer.appendChild(makedivEl);
+		answerButtons.appendChild(makedivEl);
 	} else {
 		secondsRemaining = secondsRemaining - penaltyAmount;
 		console.log("incorrect");
 		makedivEl.textContent = "That is not correct :(";
-		questionsContainer.appendChild(makedivEl);
+		answerButtons.appendChild(makedivEl);
 	}
 
 	questionIndex++;
 
 	if (questionIndex >= questionBank.length) {
 		gameOver();
-		makedivEL.textContent = "That is the end";
 	} else {
 		showQuestions(questionIndex);
 	}
@@ -84,5 +93,57 @@ function checkAnswer(event) {
 }
 
 function gameOver() {
+	questionsContainer.innerHTML = "Game Over";
+	answerButtons.innerHTML = "";
+	var makebuttonEl = document.createElement("button");
+	makebuttonEl.innerText = "Start Over?";
+	answerButtons.appendChild(makebuttonEl);
 	console.log("Game Over");
+
+	if (secondsRemaining >= 0) {
+		var remainingTime = secondsRemaining;
+		var createh2 = document.createElement("h2");
+		clearInterval(penaltyTime);
+		createh2.textContent = "Your Final Score: " + remainingTime;
+		questionsContainer.appendChild(createh2);
+	}
+	questionIndex = 0;
+
+	var makeForm = document.createElement("form");
+	var makeInput = document.createElement("input");
+	makeInput.setAttribute("id", "userInitials");
+	var makeformLabel = document.createElement("label");
+	var makesubmitButton = document.createElement("button");
+	makeformLabel.textContent = "Enter initials:";
+	makesubmitButton.textContent = "Submit";
+	questionsContainer.appendChild(makeformLabel);
+	makeForm.appendChild(makesubmitButton);
+	makeForm.appendChild(makeInput);
+	questionsContainer.appendChild(makeForm);
+
+	makesubmitButton.addEventListener("click", function () {
+		var getInitials = document.getElementById("userInitials");
+		userInitials = getInitials.value;
+
+		if (userInitials === null) {
+			console.log("No value entered");
+		} else {
+			var playerRecord = {
+				initials: userInitials,
+				score: remainingTime,
+			};
+			var totalScore = localStorage.getItem("totalScore");
+			if (totalScore === null) {
+				totalScore = [];
+			} else {
+				totalScore = JSON.parse(totalScore);
+			}
+			totalScore.push(playerRecord);
+			var newScores = JSON.stringify(totalScore);
+			localStorage.setItem("totalScore", newScores);
+			makeInput.value = "";
+		}
+		questionsContainer.textContent = "Thank you!";
+	});
+	makebuttonEl.addEventListener("click", showQuestions);
 }
